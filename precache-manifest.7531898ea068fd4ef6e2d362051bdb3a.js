198378a093719ca41d9e38748e5ad2c40b34db32
damolew19 <d.lewinski95@gmail.com>
2020-04-21 11:39:11 +1000

    Updates

---
+++ precache-manifest.7531898ea068fd4ef6e2d362051bdb3a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6472218710f8778d95567a7c6b74207",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "95fd3746324ca8daa849",
+    "url": "/portfolio/static/css/main.04eae82f.chunk.css"
+  },
+  {
+    "revision": "8535b58e20fe11ca7edb",
+    "url": "/portfolio/static/js/2.30361a48.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.30361a48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95fd3746324ca8daa849",
+    "url": "/portfolio/static/js/main.534e7272.chunk.js"
+  },
+  {
+    "revision": "1e74db4740f99c68a0bf",
+    "url": "/portfolio/static/js/runtime-main.b37861ed.js"
+  },
+  {
+    "revision": "bc5602e04c58e72aa8ed029dd223d193",
+    "url": "/portfolio/static/media/dtnThumbnail.bc5602e0.png"
+  },
+  {
+    "revision": "0ec28c88a7ba34296a52ae8fa89011ac",
+    "url": "/portfolio/static/media/home.0ec28c88.png"
+  },
+  {
+    "revision": "f970eca4e09ceec54bb57246e4dac812",
+    "url": "/portfolio/static/media/hpThumbnail.f970eca4.png"
+  },
+  {
+    "revision": "f9fe2fa7f2cd6da100f0fbe4234bca47",
+    "url": "/portfolio/static/media/me.f9fe2fa7.jpg"
+  }
+]);
